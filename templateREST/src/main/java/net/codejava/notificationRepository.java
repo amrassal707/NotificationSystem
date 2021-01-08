57238978b82a231025cafd37d83e9e2c20368f27
Abdelrahman Esmat <60976246+Esmat2020@users.noreply.github.com>
2021-01-08 21:58:36 +0200

    Abdelrahman Esmat

---
+++ templateREST/src/main/java/net/codejava/notificationRepository.java
@@ -0,0 +1,8 @@
+package net.codejava;
+ 
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface notificationRepository extends JpaRepository<notification, Integer> {
+	
+ 
+}
