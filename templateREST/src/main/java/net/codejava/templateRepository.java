0e754163ce36a9ef323e57d311f7be243c3f5f3f
amras <amras@192.168.1.13>
2021-01-08 18:51:51 +0200

    start

--- templateREST/src/main/java/net/codejava/templateRepository.java
+++
@@ -1,7 +0,0 @@
-package net.codejava;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-// a simple interface for repository actions and crud operations
-public interface templateRepository  extends JpaRepository<template, Integer>{
-
-}
